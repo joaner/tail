41deb5e9c6babd637c3a725368eb59e798049fcc
xiaokang pu <pxk@seiue.com>
2018-06-10 23:37:22 +0800

    split format

--- src/format/factory.js
+++ src/format/factory.js
@@ -1,10 +1,9 @@
 const url = require('url')
 const path = require('path')
 
-const FileDriver = require('./file')
-const NetcatDriver = require('./netcat')
+const TextFormat = require('./text')
 
-class DriverFactory {
+class FormatFactory {
   constructor(path) {
     this.parse = url.parse(path)
     this.params = new URLSearchParams(this.parse.query)
@@ -17,10 +16,13 @@ class DriverFactory {
   getInstance(uri) {
     let instance
 
-    if (this.parse.pathname.indexOf('netcat:') === 1) {
-      instance = new NetcatDriver(this.parse.pathname, this.params)
-    } else {
-      instance = new FileDriver(this.parse.pathname, this.params)
+    const extname = path.extname(this.parse.pathname).slice(1)
+    switch (extname) {
+      case 'txt':
+      case 'log':
+      default:
+        instance = new TextDriver()
+        break
     }
 
     return instance
